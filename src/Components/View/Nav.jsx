946737d3da7543458a5cc405e87cd62bdc6032eb
HiK <restartplant@gmail.com>
2019-08-31 23:17:35 +0900

    logo change

--- src/Components/View/Nav.jsx
+++ src/Components/View/Nav.jsx
@@ -1,9 +1,10 @@
 import React from 'react';
+import logo from '../../icon/logo.jpg';
 
 const Nav = ({ user }) => {
 	return(
 		<div className="bg nav">
-			<a href="/" className="txt-white logo">SICKO MODE</a>
+			<a href="/" className="txt-white logo"><img className="logoI" src={logo} />SICKO MODE</a>
   			<div className="top-btn">
 	{user.name !== '' ?  <div className="txt-white"><span>{user.name}</span><span>({user.category})</span><a className="txt-white">Log out</a></div> : <a href='/login'><span className="txt-white">Login</span></a>}
   			</div>
